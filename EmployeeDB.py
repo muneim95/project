0636dd05b2f6d1614a792818fea0f6b2b18d2c60
muneim95 <111426985+muneim95@users.noreply.github.com>
2022-10-09 14:42:41 +0100

    Add files via upload

---
+++ EmployeeDB.py
@@ -0,0 +1,121 @@
+
+import sqlite3  
+  
+con = sqlite3.connect("employee.db")  
+print("Database opened successfully")  
+  
+con.execute("create table if not exists Employees (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, email TEXT UNIQUE NOT NULL, address TEXT NOT NULL)")  
+  
+print("Table created successfully")  
+  
+con.close()
+from flask import Flask, render_template, request, redirect, flash, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+
+
+
+app = Flask(__name__)
+IS_DEV = app.env == 'development'  # FLASK_ENV env. variable
+from flask import Flask, render_template, request, redirect, flash, url_for
+import os
+from flask_sqlalchemy import SQLAlchemy
+import sqlite3
+
+
+from flask import *  
+import sqlite3  
+  
+app = Flask(__name__)  
+     
+
+@app.route("/")  
+def index():  
+    return render_template("index.html");  
+ 
+@app.route("/add")  
+def add():  
+    return render_template("add.html")  
+ 
+@app.route("/savedetails",methods = ["POST","GET"])  
+def saveDetails():  
+    msg = "msg"  
+    if request.method == "POST":  
+        try:  
+            name = request.form["name"]  
+            email = request.form["email"]  
+            address = request.form["address"]  
+            with sqlite3.connect("employee.db") as con:  
+                cur = con.cursor()  
+                cur.execute("INSERT into Employees (name, email, address) values (?,?,?)",(name,email,address))  
+                con.commit()  
+                msg = "Employee successfully Added"  
+        except:  
+            con.rollback()  
+            msg = "We can not add the employee to the list"  
+        finally:  
+            return render_template("success.html",msg = msg)  
+            con.close()  
+ 
+@app.route("/view")  
+def view():  
+    con = sqlite3.connect("employee.db")  
+    con.row_factory = sqlite3.Row  
+    cur = con.cursor()  
+    cur.execute("select * from Employees")  
+    rows = cur.fetchall()  
+    return render_template("view.html",rows = rows)  
+ 
+ 
+@app.route("/delete")  
+def delete():  
+    return render_template("delete.html")  
+ 
+@app.route("/deleterecord",methods = ["POST"])  
+def deleterecord():  
+    id = request.form["id"]  
+    with sqlite3.connect("employee.db") as con:  
+        try:  
+            cur = con.cursor()  
+            cur.execute("delete from Employees where id = ?",id)  
+            msg = "record successfully deleted"  
+        except:  
+            msg = "can't be deleted"  
+        finally:  
+            return render_template("delete_record.html",msg = msg)  
+
+if __name__ == "__main__":  
+    app.run(debug = True)  
+  
+
+
+ 
+# code
+
+if __name__ == '__main__':
+    # guaranteed to not be run on a production server
+    os.environ['FLASK_ENV'] = 'development'  # HARD CODE since default is production
+    
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+   return render_template('index.html')
+   
+
+
+if __name__ == '__main__':
+    app.run('127.0.0.1', port=5000)
+    
+
+def api_response():
+    from flask import jsonify
+    if request.method == 'POST':
+        return jsonify(**request.json)
+        app.run('127.0.0.1', port=5000)
+
+
